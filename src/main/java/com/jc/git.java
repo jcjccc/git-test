444ba1ac1168c3606c5c77c3e159c41b4853c51e
JC <374847838@qq.com>
2022-06-10 02:50:15 +0800

    first commit

--- src/main/java/com/jc/git.java
+++ src/main/java/com/jc/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hello git~");
         System.out.println("hello git~");
         System.out.println("hello git~");
+        System.out.println("git@github.com:jcjccc/JC.git");
     }
 }
